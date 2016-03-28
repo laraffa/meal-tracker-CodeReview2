b6ac98586fc5e2cd3437b3502da9e3e9fc79d33b
lindsey <lindsey.raffa@gmail.com>
2016-03-27 22:47:39 -0700

    edit model

--- app/meal.model.ts
+++ app/meal.model.ts
@@ -1,5 +1,7 @@
 export class Meal {
-  public healthy: boolean = false;
-  constructor(public meal: string, public description: string, public calories: number) {
+    public low: boolean = false;
+
+    constructor(public mealName: string, public description: string, public calories: number){
+
+    }
   }
-}
